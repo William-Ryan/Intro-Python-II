3152858ec712f085cd8465ae82d1d4be94d1b744
William <billy.ryan.rom@gmail.com>
2020-07-09 17:35:35 -0400

    Add Adventure class and user flow to adv.py * Add Room class to room.py * Add init and functions to the following classes

--- src/adv.py
+++ src/adv.py
@@ -1,37 +1,37 @@
 from room import Room
 
-# Declare all the rooms
+# # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+# room = {
+#     'outside':  Room("Outside Cave Entrance",
+#                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+# passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+# to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south."""),
+# }
 
 
-# Link rooms together
+# # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
 
 #
 # Main
@@ -49,3 +49,57 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+class Adventure():
+    def __init__(self, player_name, current_room = "Outside", current_description = "Outside Cave Entrance"):
+        self.p_name = player_name
+        self.c_room = current_room
+        self.c_description = current_description
+    
+    def inputName(self, name):
+        if(len(name) < 3 or len(name) > 20):
+            print("Invalid Name. Name must be between 3 and 20 letters. Try Again!")
+        else:
+            self.p_name = name
+            return self.p_name
+    
+    def moveLocation(self, direction):
+        if(direction == "n"):
+            print(f"You traveled North")
+        elif(direction == "e"):
+            print(f"You traveled East")
+        elif(direction == "s"):
+            print(f"You traveled South")
+        elif(direction == "w" ):
+            print(f"You traveled West")
+        else:
+            print("That doesn't lead anywhere")
+
+
+name = input("Welcome Player! Please input your name here: ") 
+name = str(name)
+
+player_account = Adventure(name)
+
+# print(player_account.p_name)
+
+if(player_account.p_name):
+    player_account.inputName(player_account.p_name)
+    if(len(name) < 3 or len(name) > 20):
+        pass
+    else:
+        print("---------------------------")
+        print(f"Congratulations {player_account.p_name}! Enjoy the game!")
+        print("---------------------------")
+        print("---------------------------") 
+        print(f"{player_account.p_name} this is Room {player_account.r_name}")
+        print("---------------------------")
+        print(f"Room Description: {player_account.r_description}")
+        print("---------------------------")
+        location = input("Choose a direction North[n], South[s], East[e], West[w]: ")
+        location = str(location)
+        print("---------------------------")
+        player_account.moveLocation(location)
+        
+else:
+    print("No name entered. Try again!")
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+class Room(): 
+    def __init__(self, name, description):
+        self.r_name = name
+        self.r_desc = description
+    
+    def whichRoom(self, room_name, direction):
+        if(room_name == "Outside"):
+            self.r_name = room_name
+            print(f"this room's name is {self.r_name}")
+            self.r_desc = "Outside Cave Entrance. North of you, the cave mount beckons"
+            print(f"Room Description {self.r_desc}")
+            if(direction == "n"):
+                self.r_name == "Foyer"
+                self.r_desc = "Dim light filters in from the south. Dusty passages run north and east."
+            else:
+                print("That leads nowhere.")
+        else:
+            print("This room does not exist")
+
+            
