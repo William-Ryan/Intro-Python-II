ce7ab568af83d3fa5537dad3653c82bdc3ffa985
William <billy.ryan.rom@gmail.com>
2020-07-10 21:55:45 -0400

    Update adv.py class and class functions/Update room.py class and class functions

--- src/room.py
+++ src/room.py
@@ -1,20 +1,32 @@
-class Room(): 
-    def __init__(self, name, description):
-        self.r_name = name
-        self.r_desc = description
-    
-    def whichRoom(self, room_name, direction):
-        if(room_name == "Outside"):
-            self.r_name = room_name
-            print(f"this room's name is {self.r_name}")
-            self.r_desc = "Outside Cave Entrance. North of you, the cave mount beckons"
-            print(f"Room Description {self.r_desc}")
-            if(direction == "n"):
-                self.r_name == "Foyer"
-                self.r_desc = "Dim light filters in from the south. Dusty passages run north and east."
-            else:
-                print("That leads nowhere.")
-        else:
-            print("This room does not exist")
+class Room: 
+    def __init__(self, room_name = "outside", room_description = "Outside Cave Entrance"):
+        self.r_name = room_name
+        self.r_desc = room_description
 
-            
+    def changeRoom(self, name, direction):
+        if(name == "outside" and direction == "n"):
+            self.r_name = "foyer"
+            self.r_desc = "Dim light filters in from the south. Dusty passages run north and east."
+        elif(name == "foyer" and direction == "n"):
+            self.r_name = "overlook"
+            self.r_desc = "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."
+        elif(name == "foyer" and direction == "s"):
+            self.r_name = "outside"
+            self.r_desc = "Dim light filters in from the south. Dusty passages run north and east."
+        elif(name == "foyer" and direction == "e"):
+            self.r_name = "narrow"
+            self.r_desc = "The narrow passage bends here from west to north. The smell of gold permeates the air."
+        elif(name == "overlook" and direction == "s"):
+            self.r_name = "foyer"
+            self.r_desc = "Dim light filters in from the south. Dusty passages run north and east."
+        elif(name == "narrow" and direction == "w"):
+            self.r_name = "foyer"
+            self.r_desc = "Dim light filters in from the south. Dusty passages run north and east."
+        elif(name == "narrow" and direction == "n"):
+            self.r_name = "treasure"
+            self.r_desc = "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."
+        elif(name == "treasure" and direction == "n"):
+            self.r_name = "narrow"
+            self.r_desc = "The narrow passage bends here from west to north. The smell of gold permeates the air."
+        else:
+            print("This is a deadend.")
